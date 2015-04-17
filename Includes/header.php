22a17825a83075fdc8f57becf95fdb5afeac43e3
Benoit Mallo <benoit.mallo@bappli.com>
2015-04-17 11:00:43 +0200

    Add uikit framwork

---
+++ Includes/header.php
@@ -0,0 +1,53 @@
+<!doctype HTML>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv=Content-Type content="text/html; charset=utf-8" />
+	<title>Blog-sfam</title>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/uikit.js"></script>
+	<script type="text/javascript" src="js/slideshow.js"></script>
+	<link rel="stylesheet" href="css/uikit.sfam.css">
+	<link rel="stylesheet" href="css/uikit.blogsfam.css">
+
+</head>
+<body>
+<div id="tm-header">
+	<div class="uk-container uk-container-center">
+		<nav class="uk-navbar uk-visible-small">
+			<a href="#tm-offcanvas" class="uk-navbar-toggle" data-uk-offcanvas></a>
+			<div class="tm-logo-small uk-navbar-content uk-navbar-center">
+				<a href="" class="uk-navbar-brand"><img src="img/logo-small.png" alt=""></a>
+			</div>
+		</nav>
+
+		<div class="tm-toolbar uk-grid uk-position-relative uk-hidden-small">
+			<div class="tm-logo-big uk-position-top-left uk-hidden-small">
+				<img src="img/sfam-assurance-blog-logo.jpg" alt="SFAM Assurances, le blog.">
+			</div>
+			<div class="uk-flex uk-position-top-right ">
+				<!-- BLOC BEGIN-->
+				<ul class="tm-toobar-nav uk-subnav uk-subnav-line">
+					<li>Site officiel SFAM</li>
+					<li>Le Service client SFAM</li>
+				</ul>
+				<form class="uk-form uk-margin-remove uk-display-inline-block" data-uk-search>
+					<input class="uk-search-field" type="search" placeholder="SEARCH">
+				</form>
+				<!-- BLOC END-->
+			</div>
+		</div>
+
+		<nav class="uk-navbar uk-hidden-small">
+			<div class="uk-navbar-flip">
+				<ul class="uk-navbar-nav">
+					<li><a href="#">accueil</a></li>
+					<li><a href="#">a propos de sfam</a></li>
+					<li><a href="#">contact</a></li>
+				</ul>
+
+			</div>
+		</nav>
+
+	</div>
+</div>
